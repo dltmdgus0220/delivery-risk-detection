98b79da21697947fa1219ea2483546beb08e023f
dltmdgus0220 <dltmdgus@khu.ac.kr>
2025-12-31 13:10:01 +0900

    chore: 데이터 수집 설정 변경 및 데이터량 증대

--- src/data_collect.py
+++ src/data_collect.py
@@ -4,15 +4,10 @@ import pandas as pd
 
 # --- 0. 상수선언 ---
 
-PACKAGE_LIST = ['com.sampleapp','com.coupang.mobile.eats','com.fineapp.yogiyo','com.shinhan.o2o'] # 배민, 쿠팡이츠, 요기요, 땡겨요
-PACKAGE_NAME_KOR = ['배달의민족', '쿠팡이츠', '요기요', '땡겨요']
-PACKAGE_NAME_ENG = ['baemin', 'coupangeats', 'yogiyo', 'ddangyo']
-PACKAGE_NUM = 3
-NUM_DATA = 7000
-
-APP_ID = PACKAGE_LIST[PACKAGE_NUM]
-APP_NAME_KOR = PACKAGE_NAME_KOR[PACKAGE_NUM]
-APP_NAME_ENG = PACKAGE_NAME_ENG[PACKAGE_NUM]
+APP_ID = 'com.sampleapp'
+APP_NAME_KOR = '배달의민족'
+APP_NAME_ENG = 'baemin'
+NUM_DATA = 100000
 
 
 # --- 1. 데이터 수집 ---
