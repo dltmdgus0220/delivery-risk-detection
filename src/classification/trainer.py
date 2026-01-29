a24daad6934332e5b14e3eda899a8cc96593da22
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-29 14:04:45 +0900

    feat: train 함수 구현

--- src/classification/trainer.py
+++ src/classification/trainer.py
@@ -3,3 +3,25 @@ import torch
 from tqdm import tqdm
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
+
+# train
+def train_one_epoch(model, loader, optimizer, device):
+    model.train()
+    total_loss = 0.0
+
+    for batch in tqdm(loader, desc="Train", leave=True):
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss / max(1, len(loader)) # 0으로 나누는 거 방지
+
