fc1c8e3c103cba2bf45877eb91c9ccccd19c92b2
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-02-04 16:39:43 +0900

    feat: 키워드 포맷 변환 함수 및 string 정재 함수 생성

--- src/risk_analysis/summary_reviews.py
+++ src/risk_analysis/summary_reviews.py
@@ -9,3 +9,21 @@ import os
 
 
 EXCEPT_KEYWORD = ['앱-삭제', '앱-탈퇴']
+
+# --- 1. 기타 함수 ---
+
+def str_to_list_keyword(keywords: str) -> List[str]:
+    keywords = keywords.strip('[]')
+    if len(keywords) == 0:
+        return []
+    else:
+        return [x.strip() for x in keywords.replace("'", "").split(",")]
+
+def clean_one_line(s:str) -> str:
+    s = (s or "").strip() # 양쪽 공백 제거
+    s = s.strip().strip('"').strip("'").strip() # 양쪽 따옴표 제거
+    s = s.replace("\n", " ").replace("\r", " ") # 줄바꿈->공백 변환. 한줄로 결과 바꾸기
+    s = re.sub(r"\s+", " ", s).strip() # 연속되는 여러 개의 공백 변환
+    s = re.sub(r"^[\-\*\d\.\)\s]+", "", s).strip() # 불릿/번호 형태 제거
+    return s
+
