20949ab35597feec797acfc2a89d29bf796aa5c0
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-02-06 13:35:53 +0900

    feat: 키워드 카운팅 함수 구현

--- src/risk_summary/keyword_util.py
+++ src/risk_summary/keyword_util.py
@@ -6,3 +6,10 @@ from typing import List, Tuple
 EXCLUDE = ['앱-삭제', '앱-탈퇴']
 KEYWORD_COL = "keywords"
 
+# 키워드 카운팅
+def keyword_count(df:pd.DataFrame) -> Counter:
+    all_reviews = [k for ks in df[KEYWORD_COL] for k in ks]
+    counter = Counter(all_reviews)
+    
+    return counter
+
